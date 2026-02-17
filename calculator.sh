b4cba26d6f4540ec7c665fa5d4f052b1fd130580
aayanksinghai <aayanksinghai02@gmail.com>
2026-02-17 11:54:37 +0530

    Fixed Calculator program

--- calculator.sh
+++ calculator.sh
@@ -5,6 +5,7 @@ echo "1. Addition"
 echo "2. Subtraction"
 echo "3. Multiplication"
 echo "4. Division"
+echo "5. Remainder"
 
 echo -n "Enter First Number: "
 read a
@@ -14,13 +15,31 @@ echo -n "Enter the Choice: "
 read ch
 
 case $ch in
-   1) res=`expr $a + $b`
-   ;;
-   2) res=`expr $a - $b`
-   ;;
-   3) res=`expr $a \* $b`
-   ;;
-   4) res=`expr $a / $b`
-   ;;
+   1) res=$((a + b))
+      ;;
+   2) res=$((a - b))
+      ;;
+   3) res=$((a * b))
+      ;;
+   4) 
+      if [ "$b" -eq 0 ]; then
+         echo "Error: Division by zero"
+         exit 1
+      fi
+      res=$((a / b))
+      ;;
+   5)
+      if [ "$b" -eq 0 ]; then
+         echo "Error: Division by zero"
+         exit 1
+      fi
+      res=$((a % b))
+      ;;
+   *)
+      echo "Invalid Choice"
+      exit 1
+      ;;
 esac
+
 echo "Result : $res"
+
